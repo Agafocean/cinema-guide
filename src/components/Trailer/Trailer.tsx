0ec0a5ca00324ec7eb949c3dbaa4954a1382b297
Evgeny Agafonov <sth179@gmail.com>
2024-10-07 14:00:13 +1000

    cinema-guide v1.2

--- src/components/Trailer/Trailer.tsx
+++ src/components/Trailer/Trailer.tsx
@@ -17,7 +17,7 @@ export const Trailer = () => {
         <div className='trailer-container'>
             <div className="trailer" onClick={() => setPlaying(!playing)}>
                 <ReactPlayer className="player" url={`https://www.youtube.com/watch?v=${id}`}
-                    width="100%" height="100%" playing={playing}
+                    width="100%" height="100%" playing={playing} playsinline attr={true}
                 />
                 {playing && <svg className='player-btn' width="80" height="80" viewBox="0 0 80 80" fill="none" xmlns="http://www.w3.org/2000/svg">
                     <rect width="80" height="80" rx="40" fill="white" />
